51df29049f9d8b2e04d3057a9bc960268cebc419
Mateus Lopes <mateus140t@gmail.com>
2025-11-10 16:48:26 -0300

    adciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
-    return <h1>Título</h1>
+  return <h1>Título</h1>;
 }
 
+function teste() {
+  console.log("Teste");
+}
 
-
-
+function teste2() {
+  console.log("indentação errada");
+}
 
 export default Home;
